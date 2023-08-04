2cd74d2f14c2d743063dd8297809fb8b0f5329df
Gauravghanmode2903 <ganeshghanmode2903@gmail.com>
2023-08-04 18:01:09 +0530

    Day-2 Commplited

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final days = 30;
+  final name = "Gaurav";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog Application"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter with $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
