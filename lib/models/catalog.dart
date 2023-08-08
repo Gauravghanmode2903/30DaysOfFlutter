478d670d131523693477e8a2d0e03ece3dfbeaaa
Gauravghanmode2903 <ganeshghanmode2903@gmail.com>
2023-08-09 00:11:08 +0530

    Day-24 commited

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -4,15 +4,15 @@ class CatalogModel {
   static List<Item>? items;
 
   // Get Item by ID
-  static Item getById(int id) =>
+   Item getById(int id) =>
       items!.firstWhere((element) => element.id == id, orElse: null);
 
   // Get Item by position
-  static Item getByPosition(int pos) => items![pos];
+   Item getByPosition(int pos) => items![pos];
 }
 
 class Item {
-  final num id;
+  final int id;
   final String name;
   final String desc;
   final num price;
